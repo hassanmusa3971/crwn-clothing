4cad578adddb9368f3e23d36818d007d6c4b922b
hassanmusa3971~ <hassanmusa3971@gmail.com>
2022-04-04 15:02:29 -0700

    Added redux-persist to store user cat item

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,20 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import UserReducer from "./user/user-reducer";
 import CartReducer from "./cart/cart-reducer";
 
 
 
+const persistConfig = {
+          key: 'root',
+          storage,
+          whitelist: ['cart']
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
           user: UserReducer,
           cart: CartReducer
 });
+export default persistReducer(persistConfig, rootReducer);
+          
