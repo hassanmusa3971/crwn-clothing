5be19e0c839a39277d7758bf29dac1ee25893fed
hassanmusa3971~ <hassanmusa3971@gmail.com>
2022-04-03 22:49:32 -0700

    Add additem and removeitem to cart

--- src/components/checkout/checkout-item.component.jsx
+++ src/components/checkout/checkout-item.component.jsx
@@ -1,21 +1,33 @@
 import React from "react";
+import { connect } from "react-redux";
+import { clearItemFromCart, addItem, removeItem } from "../../redux/cart/cart.action";
 import "./checkout-item.style.scss";
 
+const CheckOutItem = ({ clearItem, cartItem, addItem, removeItem }) => {
+  const { name, imageUrl, quantity, price } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={()=> removeItem(cartItem)}>&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={()=> addItem(cartItem)}>&#10095;</div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
+    </div>
+  ); 
+};
 
-const CheckOutItem = ({cartItem: {name, imageUrl, quantity, price}}) =>(
-          <div className="checkout-item">
-                    <div className="image-container">
-                              <img src={imageUrl} alt="item"/>
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+});
 
-                    </div>
-                    <span className="name">{name}</span>
-                    <span className="quantity">{quantity}</span>
-                    <span className="price">{price}</span>
-                    <div className="remove-button">&#10005;</div>
-          
-
-          </div>
-);
-
-
-export default CheckOutItem
+export default connect(null, mapDispatchToProps)(CheckOutItem);
